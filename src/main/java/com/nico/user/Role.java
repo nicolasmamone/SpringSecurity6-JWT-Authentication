041b5fe77d90d97ba06fabfa9fd6b58a1edde2e8
nicolasmamone <nicolasmamone@gmail.com>
2024-03-12 20:35:43 -0300

    [ADD] Creación de la entidad User y Role

---
+++ src/main/java/com/nico/user/Role.java
@@ -0,0 +1,6 @@
+package com.nico.user;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/nico/user/User.java
@@ -0,0 +1,55 @@
+package com.nico.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @Column(nullable = false)
+    String username;
+    String lastname;
+    String firstname;
+    String country;
+    String password;
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
